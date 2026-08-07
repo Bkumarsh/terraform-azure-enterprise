a1b4ef3bc65d9a10eb80e62d5a4f29b4ef5c8133
bkumarsh <bablookumar.sh@gmail.com>
2026-08-07 20:26:38 +0530

    add version and optimize terraform other files

--- modules/resource-group/outputs.tf
+++ modules/resource-group/outputs.tf
@@ -1,7 +1,9 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.this.name
+output "resource_group_id" {
+  description = "ID of the Azure Resource Group"
+  value = azurerm_resource_group.rg.id
 }
 
-output "resource_group_id" {
-  value = azurerm_resource_group.this.id
+output "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  value = azurerm_resource_group.rg.name
 }
--- modules/resource-group/variable.tf
+++ modules/resource-group/variable.tf
@@ -1,9 +1,12 @@
 variable "resource_group_name" {
   description = "Name of the Resource Group"
   type        = string
+  nullable = false
 }
 
 variable "location" {
   description = "Azure Region"
   type        = string
+  nullable = false      
+  # It prevents mandatory input variables from being assigned a null value. This helps catch configuration errors during the planning phase instead of failing later during resource creation.
 }
