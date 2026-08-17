49f051493f87f47ed8a875d57f5dbce3b118f9af
bkumarsh <bablookumar.sh@gmail.com>
2026-08-18 02:13:55 +0530

    feat: add resource_group_id and resource_group_name outputs to resource group module

--- modules/resource-group/outputs.tf
+++ modules/resource-group/outputs.tf
@@ -5,5 +5,5 @@ output "resource_group_id" {
 
 output "resource_group_name" {
   description = "Name of the Azure Resource Group"
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg.this
 }
